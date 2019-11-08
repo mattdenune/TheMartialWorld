49b454270fc25fbbf0cad6dd432bf96ebdd71c69
Matthew Denune <mattdenune@gmail.com>
2019-11-08 11:57:58 -0500

    updated controller

--- Martial-World/app/controllers/arts_controller.rb
+++ Martial-World/app/controllers/arts_controller.rb
@@ -7,12 +7,62 @@ class ArtsController < ApplicationController
     def show
         begin
             @art = Art.find(params[:id])
-            render json: { message: "ok", art: @art }
+            render json: { 
+                message: "ok", 
+                art: @art 
+            }
         rescue ActiveRecord::RecordNotFound
-            render json: { message: 'no martial art matches that ID' }, status: 404
+            render json: { 
+                message: 'no martial art matches that ID' 
+                }, status: 404
         rescue StandardError => e
-            render json: { message: e.to_s }, status: 500
+            render json: { 
+                message: e.to_s 
+                }, status: 500
             
         end
     end
+
+    def create
+        puts 'creating martial art'
+        puts params
+            art = Art.new(art_params)
+        puts "??", art
+            if art.save
+                render json: {
+                    message: "ok",
+                    art: art
+                }
+            else
+                render json: {
+                    message: art.errors
+                }, status: 500
+            end
+    end
+
+    def update
+        if @art.update(art_params)
+            render json: {
+                message: @art.errors
+            }, status: 500
+        end
+    end
+
+    def destroy
+        @art.destroy
+        render json: {
+            message: "ok"
+        }
+    end
+
+    private
+
+    def set_art
+        @art = Art.fing(params[:id])
+    end
+
+    def art_params
+        params.permit(:name, :origin, :founder, :founder_img, :style, :description)
+    end
+
 end
