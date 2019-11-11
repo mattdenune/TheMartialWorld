4b47924b0c57c59bb4c02e993b5efe05721b00ae
Matthew Denune <mattdenune@gmail.com>
2019-11-11 12:23:04 -0500

    working on app.js

--- Martial-World/client/src/App.js
+++ Martial-World/client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
+import AllArts from './components/AllArts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super()
+    this.state={
+      arts: [],
+      currentArts: {},
+      artsLoaded: false
+    };
+  }
+
+  getAllArts = () => {
+    axios.get("http://localhost:3000/arts").then(jsonRes => {
+      this.setState({
+        arts: jsonRes.data.arts,
+        artsLoaded: true
+      });
+    });
+  };
+
+  setArt = (art) => {
+    this.setState({
+      currentArt: art
+    });
+  };
+
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
